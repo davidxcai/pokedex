cbd689eee8bec5eac0bd531daef1240a6e55c103
David Cai <43160344+davidxcai@users.noreply.github.com>
2025-05-05 15:08:50 -0500

    Updated files

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <QueryClientProvider client={new QueryClient()}>
+            <App />
+            <ReactQueryDevtools
+                initialIsOpen={false}
+                buttonPosition="bottom-left"
+            />
+        </QueryClientProvider>
+    </StrictMode>
+);
